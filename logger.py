d359b809f0ff1af7a75199539dbd908b36e64724
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2024-04-13 05:41:17 -0300

    refactor: class transform & corrections

--- logger.py
+++ logger.py
@@ -1,108 +1,128 @@
 import csv
-
 import keyboard
 import pygetwindow as gw
 from datetime import datetime
-
+import sys
+import ctypes
 import os
-
-windows_list_map = []
-
-with open("windows_list.csv", "r", newline="", encoding="utf-8") as csv_file:
-    reader = csv.DictReader(csv_file)
-    for row in reader:
-        windows_list_map.append(dict(row))
-    print(windows_list_map)
+import ctypes
 
 
-def save_windows_list():
-    with open("windows_list.csv", "w", newline="", encoding="utf-8") as csv_file:
-        headers = ["code", "title"]
-        writer = csv.DictWriter(csv_file, fieldnames=headers)
-
-        writer.writeheader()
-        for row in windows_list_map:
-            writer.writerow(row)
-
-
-def save_log(log):
-    if os.path.exists("log.csv"):
-        with open("log.csv", "a", newline="", encoding="utf-8") as csv_file:
-            for register in log:
-                writer = csv.writer(csv_file)
-                writer.writerow(register)
-
-    else:
-        with open("log.csv", "w", newline="", encoding="utf-8") as csv_file:
-            headers = ["window_code", "key_code"]
-            writer = csv.writer(csv_file)
-            writer.writerow(headers)
-
-            for register in log:
-                writer.writerow(register)
-
-
-# Variável global para armazenar o minuto inicial
-initial_minute = None
-
-# Função para verificar se passou um minuto desde o início do script
-def passou_um_minuto():
-    global initial_minute
-    
-    # Se o minuto inicial não foi definido, define-o agora
-    if initial_minute is None:
-        initial_minute = datetime.now().minute
-        return False  # Não passou um minuto ainda
-    
-    # Verifica se o minuto atual é diferente do minuto inicial
-    actual_minute = datetime.now().minute
-    if actual_minute != initial_minute:
-        initial_minute = actual_minute  # Atualiza o minuto inicial
-        return True  # Passou um minuto
-    else:
-        return False  # Não passou um minuto ainda
-
-
-
-log = []
-
-
-def on_key_press(event):
-    window_title = gw.getActiveWindowTitle()
-
-    if window_title not in [window["title"] for window in windows_list_map]:
-        windows_list_map.append(
-            {
-                "code": len(windows_list_map) + 1,
-                "title": str(window_title),
-            }
-        )
-        save_windows_list()
-
-        window_code = len(windows_list_map)
-    else:
-        window_code = [
-            window["code"]
-            for window in windows_list_map
-            if window["title"] == window_title
-        ][0]
-
-    print(
-        "Tecla pressionada:",
-        str(event.name).upper(),
-        "scan_code:",
-        event.scan_code,
-        "window_code:",
-        window_code,
+def message_box(text):
+    return ctypes.windll.user32.MessageBoxW(
+        0, text, "Key Logger Python - Gabriel Moscarde", 0
     )
-    log.append([event.scan_code, window_code])
-    if passou_um_minuto():
-        save_log(log)
-        print("Passou um minuto, salvando o log e zerando a lista")
-    else:
-        print("Ainda não passou um minuto")
 
 
-keyboard.on_press(on_key_press)
+class Logger:
+    def __init__(self):
+        self.log = []
+        self.windows_map_path = "map/windows_map.csv"
+        self.windows_map = self.read_windows_map()
+        self.path_log = self.create_path_log()
+        self.initial_minute = datetime.now().minute
 
-keyboard.wait()
+    def read_windows_map(self):
+        if os.path.exists(self.windows_map_path):
+            windows_map = []
+            with open(
+                self.windows_map_path, "r", newline="", encoding="utf-8"
+            ) as csv_file:
+                reader = csv.DictReader(csv_file)
+                for row in reader:
+                    windows_map.append(dict(row))
+
+                return windows_map
+
+    def start(self):
+        keyboard.on_press(self.on_key_press)
+        keyboard.wait()
+
+    def on_key_press(self, event):
+        window_title = gw.getActiveWindowTitle()
+        if len([key for key in self.log if key[0] == 1]) > 100:
+            keyboard.unhook_all()
+            message_box("Stopping logger...")
+            sys.exit()
+
+        if window_title not in [window["title"] for window in self.windows_map]:
+            self.windows_map.append(
+                {
+                    "code": len(self.windows_map) + 1,
+                    "title": str(window_title),
+                }
+            )
+            window_code = len(self.windows_map)
+        else:
+            window_code = [
+                window["code"]
+                for window in self.windows_map
+                if window["title"] == window_title
+            ][0]
+
+        print(
+            "Key pressed:",
+            str(event.name).upper(),
+            "scan_code:",
+            event.scan_code,
+            "window_code:",
+            window_code,
+        )
+
+        self.log.append([event.scan_code, window_code])
+
+        if self.minute_passed():
+            self.save_log(self.log)
+            self.log = []
+            print("Logging...")
+
+    def minute_passed(self):
+        actual_minute = datetime.now().minute
+
+        if actual_minute != self.initial_minute:
+            self.initial_minute = actual_minute
+            return True
+        else:
+            return False
+
+    def save_log(self, log):
+        self.save_windows_map()
+
+        if os.path.exists(self.path_log):
+            with open(self.path_log, "a", newline="", encoding="utf-8") as csv_file:
+                for register in log:
+                    writer = csv.writer(csv_file)
+                    writer.writerow(register)
+
+        else:
+            with open(self.path_log, "w", newline="", encoding="utf-8") as csv_file:
+                headers = ["window_code", "key_code"]
+                writer = csv.writer(csv_file)
+                writer.writerow(headers)
+
+                for register in log:
+                    writer.writerow(register)
+
+    def create_path_log(self):
+        date = datetime.now().strftime("%d-%m-%Y")
+        return f"logs/log_{date}.csv"
+
+    def save_windows_map(self):
+        with open(self.windows_map_path, "w", newline="", encoding="utf-8") as csv_file:
+            headers = ["code", "title"]
+            writer = csv.DictWriter(csv_file, fieldnames=headers)
+
+            writer.writeheader()
+            for row in self.windows_map:
+                writer.writerow(row)
+
+
+if __name__ == "__main__":
+    message_box(
+        """Starting logger...
+To stop press ESC for 5 seconds!
+
+Press OK to continue..."""
+    )
+    logger = Logger()
+    logger.start()
